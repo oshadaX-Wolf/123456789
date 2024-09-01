3f397529ea20cbe8a9b69c2e996d6b1987e86ba7
oshadaX-Wolf <153474645+oshadaX-Wolf@users.noreply.github.com>
2024-09-02 05:02:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const { ttdl } = require("btch-downloader");
 const { VideoDownload } = require("./database"); // Update path as necessary
